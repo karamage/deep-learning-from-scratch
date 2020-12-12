535a7a801f1313985b4c969051a69e4d2879587a
karamage <karamage.umauma@gmail.com>
2020-12-12 16:56:18 +0900

    add gradient simple net.

---
+++ ch04/gradient_simplenet.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3) #ガウス分布で初期化
+    def predict(self, x):
+        return np.dot(x, self.W)
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+x = np.array([0.6, 0.9])
+t = np.array([0, 0, 1])
+
+net = simpleNet()
+
+print(net.W)
+
+p = net.predict(x)
+print(p)
+
+f = lambda w: net.loss(x, t)
+dW = numerical_gradient(f, net.W)
+
+print(dW)
