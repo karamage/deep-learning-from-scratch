a9a1550c83360160dd8c2331d3f662e53fd0bd1b
karamage <karamage.umauma@gmail.com>
2020-12-20 15:53:40 +0900

    improve sigmoid.

--- common/_multi_layer_net.py
+++ common/_multi_layer_net.py
@@ -1,15 +1,14 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir) # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 from collections import OrderedDict
-from common.layers import *
+from common._layers import *
 from common.gradient import numerical_gradient
 
-class MultiLayerNetExtend:
-    """拡張版の全結合による多層ニューラルネットワーク
-    
-    Weiht Decay、Dropout、Batch Normalizationの機能を持つ
+
+class MultiLayerNet:
+    """全結合による多層ニューラルネットワーク
 
     Parameters
     ----------
@@ -21,20 +20,14 @@ class MultiLayerNetExtend:
         'relu'または'he'を指定した場合は「Heの初期値」を設定
         'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
     weight_decay_lambda : Weight Decay（L2ノルム）の強さ
-    use_dropout: Dropoutを使用するかどうか
-    dropout_ration : Dropoutの割り合い
-    use_batchNorm: Batch Normalizationを使用するかどうか
     """
     def __init__(self, input_size, hidden_size_list, output_size,
-                 activation='relu', weight_init_std='relu', weight_decay_lambda=0, 
-                 use_dropout = False, dropout_ration = 0.5, use_batchnorm=False):
+                 activation='relu', weight_init_std='relu', weight_decay_lambda=0):
         self.input_size = input_size
         self.output_size = output_size
         self.hidden_size_list = hidden_size_list
         self.hidden_layer_num = len(hidden_size_list)
-        self.use_dropout = use_dropout
         self.weight_decay_lambda = weight_decay_lambda
-        self.use_batchnorm = use_batchnorm
         self.params = {}
 
         # 重みの初期化
@@ -46,18 +39,11 @@ class MultiLayerNetExtend:
         for idx in range(1, self.hidden_layer_num+1):
             self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
                                                       self.params['b' + str(idx)])
-            if self.use_batchnorm:
-                self.params['gamma' + str(idx)] = np.ones(hidden_size_list[idx-1])
-                self.params['beta' + str(idx)] = np.zeros(hidden_size_list[idx-1])
-                self.layers['BatchNorm' + str(idx)] = BatchNormalization(self.params['gamma' + str(idx)], self.params['beta' + str(idx)])
-                
             self.layers['Activation_function' + str(idx)] = activation_layer[activation]()
-            
-            if self.use_dropout:
-                self.layers['Dropout' + str(idx)] = Dropout(dropout_ration)
 
         idx = self.hidden_layer_num + 1
-        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)], self.params['b' + str(idx)])
+        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+            self.params['b' + str(idx)])
 
         self.last_layer = SoftmaxWithLoss()
 
@@ -77,33 +63,39 @@ class MultiLayerNetExtend:
                 scale = np.sqrt(2.0 / all_size_list[idx - 1])  # ReLUを使う場合に推奨される初期値
             elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
                 scale = np.sqrt(1.0 / all_size_list[idx - 1])  # sigmoidを使う場合に推奨される初期値
+
             self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx-1], all_size_list[idx])
             self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
 
-    def predict(self, x, train_flg=False):
-        for key, layer in self.layers.items():
-            if "Dropout" in key or "BatchNorm" in key:
-                x = layer.forward(x, train_flg)
-            else:
-                x = layer.forward(x)
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
         return x
 
-    def loss(self, x, t, train_flg=False):
+    def loss(self, x, t):
         """損失関数を求める
-        引数のxは入力データ、tは教師ラベル
+
+        Parameters
+        ----------
+        x : 入力データ
+        t : 教師ラベル
+
+        Returns
+        -------
+        損失関数の値
         """
-        y = self.predict(x, train_flg)
+        y = self.predict(x)
 
         weight_decay = 0
         for idx in range(1, self.hidden_layer_num + 2):
             W = self.params['W' + str(idx)]
-            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W**2)
+            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W ** 2)
 
         return self.last_layer.forward(y, t) + weight_decay
 
     def accuracy(self, x, t):
-        y = self.predict(x, train_flg=False)
+        y = self.predict(x)
         y = np.argmax(y, axis=1)
         if t.ndim != 1 : t = np.argmax(t, axis=1)
 
@@ -124,22 +116,31 @@ class MultiLayerNetExtend:
             grads['W1']、grads['W2']、...は各層の重み
             grads['b1']、grads['b2']、...は各層のバイアス
         """
-        loss_W = lambda W: self.loss(x, t, train_flg=True)
+        loss_W = lambda W: self.loss(x, t)
 
         grads = {}
         for idx in range(1, self.hidden_layer_num+2):
             grads['W' + str(idx)] = numerical_gradient(loss_W, self.params['W' + str(idx)])
             grads['b' + str(idx)] = numerical_gradient(loss_W, self.params['b' + str(idx)])
-            
-            if self.use_batchnorm and idx != self.hidden_layer_num+1:
-                grads['gamma' + str(idx)] = numerical_gradient(loss_W, self.params['gamma' + str(idx)])
-                grads['beta' + str(idx)] = numerical_gradient(loss_W, self.params['beta' + str(idx)])
 
         return grads
-        
+
     def gradient(self, x, t):
+        """勾配を求める（誤差逆伝搬法）
+
+        Parameters
+        ----------
+        x : 入力データ
+        t : 教師ラベル
+
+        Returns
+        -------
+        各層の勾配を持ったディクショナリ変数
+            grads['W1']、grads['W2']、...は各層の重み
+            grads['b1']、grads['b2']、...は各層のバイアス
+        """
         # forward
-        self.loss(x, t, train_flg=True)
+        self.loss(x, t)
 
         # backward
         dout = 1
@@ -153,11 +154,7 @@ class MultiLayerNetExtend:
         # 設定
         grads = {}
         for idx in range(1, self.hidden_layer_num+2):
-            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.params['W' + str(idx)]
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.layers['Affine' + str(idx)].W
             grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
-
-            if self.use_batchnorm and idx != self.hidden_layer_num+1:
-                grads['gamma' + str(idx)] = self.layers['BatchNorm' + str(idx)].dgamma
-                grads['beta' + str(idx)] = self.layers['BatchNorm' + str(idx)].dbeta
 
         return grads
--- common/_multi_layer_net_extend.py
+++ common/_multi_layer_net_extend.py
@@ -1,14 +1,15 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir) # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 from collections import OrderedDict
-from common.layers import *
+from common._layers import *
 from common.gradient import numerical_gradient
 
-
-class MultiLayerNet:
-    """全結合による多層ニューラルネットワーク
+class MultiLayerNetExtend:
+    """拡張版の全結合による多層ニューラルネットワーク
+    
+    Weiht Decay、Dropout、Batch Normalizationの機能を持つ
 
     Parameters
     ----------
@@ -20,14 +21,20 @@ class MultiLayerNet:
         'relu'または'he'を指定した場合は「Heの初期値」を設定
         'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
     weight_decay_lambda : Weight Decay（L2ノルム）の強さ
+    use_dropout: Dropoutを使用するかどうか
+    dropout_ration : Dropoutの割り合い
+    use_batchNorm: Batch Normalizationを使用するかどうか
     """
     def __init__(self, input_size, hidden_size_list, output_size,
-                 activation='relu', weight_init_std='relu', weight_decay_lambda=0):
+                 activation='relu', weight_init_std='relu', weight_decay_lambda=0, 
+                 use_dropout = False, dropout_ration = 0.5, use_batchnorm=False):
         self.input_size = input_size
         self.output_size = output_size
         self.hidden_size_list = hidden_size_list
         self.hidden_layer_num = len(hidden_size_list)
+        self.use_dropout = use_dropout
         self.weight_decay_lambda = weight_decay_lambda
+        self.use_batchnorm = use_batchnorm
         self.params = {}
 
         # 重みの初期化
@@ -39,11 +46,18 @@ class MultiLayerNet:
         for idx in range(1, self.hidden_layer_num+1):
             self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
                                                       self.params['b' + str(idx)])
+            if self.use_batchnorm:
+                self.params['gamma' + str(idx)] = np.ones(hidden_size_list[idx-1])
+                self.params['beta' + str(idx)] = np.zeros(hidden_size_list[idx-1])
+                self.layers['BatchNorm' + str(idx)] = BatchNormalization(self.params['gamma' + str(idx)], self.params['beta' + str(idx)])
+                
             self.layers['Activation_function' + str(idx)] = activation_layer[activation]()
+            
+            if self.use_dropout:
+                self.layers['Dropout' + str(idx)] = Dropout(dropout_ration)
 
         idx = self.hidden_layer_num + 1
-        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
-            self.params['b' + str(idx)])
+        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)], self.params['b' + str(idx)])
 
         self.last_layer = SoftmaxWithLoss()
 
@@ -63,39 +77,33 @@ class MultiLayerNet:
                 scale = np.sqrt(2.0 / all_size_list[idx - 1])  # ReLUを使う場合に推奨される初期値
             elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
                 scale = np.sqrt(1.0 / all_size_list[idx - 1])  # sigmoidを使う場合に推奨される初期値
-
             self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx-1], all_size_list[idx])
             self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
 
-    def predict(self, x):
-        for layer in self.layers.values():
-            x = layer.forward(x)
+    def predict(self, x, train_flg=False):
+        for key, layer in self.layers.items():
+            if "Dropout" in key or "BatchNorm" in key:
+                x = layer.forward(x, train_flg)
+            else:
+                x = layer.forward(x)
 
         return x
 
-    def loss(self, x, t):
+    def loss(self, x, t, train_flg=False):
         """損失関数を求める
-
-        Parameters
-        ----------
-        x : 入力データ
-        t : 教師ラベル
-
-        Returns
-        -------
-        損失関数の値
+        引数のxは入力データ、tは教師ラベル
         """
-        y = self.predict(x)
+        y = self.predict(x, train_flg)
 
         weight_decay = 0
         for idx in range(1, self.hidden_layer_num + 2):
             W = self.params['W' + str(idx)]
-            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W ** 2)
+            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W**2)
 
         return self.last_layer.forward(y, t) + weight_decay
 
     def accuracy(self, x, t):
-        y = self.predict(x)
+        y = self.predict(x, train_flg=False)
         y = np.argmax(y, axis=1)
         if t.ndim != 1 : t = np.argmax(t, axis=1)
 
@@ -116,31 +124,22 @@ class MultiLayerNet:
             grads['W1']、grads['W2']、...は各層の重み
             grads['b1']、grads['b2']、...は各層のバイアス
         """
-        loss_W = lambda W: self.loss(x, t)
+        loss_W = lambda W: self.loss(x, t, train_flg=True)
 
         grads = {}
         for idx in range(1, self.hidden_layer_num+2):
             grads['W' + str(idx)] = numerical_gradient(loss_W, self.params['W' + str(idx)])
             grads['b' + str(idx)] = numerical_gradient(loss_W, self.params['b' + str(idx)])
+            
+            if self.use_batchnorm and idx != self.hidden_layer_num+1:
+                grads['gamma' + str(idx)] = numerical_gradient(loss_W, self.params['gamma' + str(idx)])
+                grads['beta' + str(idx)] = numerical_gradient(loss_W, self.params['beta' + str(idx)])
 
         return grads
-
+        
     def gradient(self, x, t):
-        """勾配を求める（誤差逆伝搬法）
-
-        Parameters
-        ----------
-        x : 入力データ
-        t : 教師ラベル
-
-        Returns
-        -------
-        各層の勾配を持ったディクショナリ変数
-            grads['W1']、grads['W2']、...は各層の重み
-            grads['b1']、grads['b2']、...は各層のバイアス
-        """
         # forward
-        self.loss(x, t)
+        self.loss(x, t, train_flg=True)
 
         # backward
         dout = 1
@@ -154,7 +153,11 @@ class MultiLayerNet:
         # 設定
         grads = {}
         for idx in range(1, self.hidden_layer_num+2):
-            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.layers['Affine' + str(idx)].W
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.params['W' + str(idx)]
             grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+
+            if self.use_batchnorm and idx != self.hidden_layer_num+1:
+                grads['gamma' + str(idx)] = self.layers['BatchNorm' + str(idx)].dgamma
+                grads['beta' + str(idx)] = self.layers['BatchNorm' + str(idx)].dbeta
 
         return grads
