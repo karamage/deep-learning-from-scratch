a55c8aded0d2ac6e0a5f7f089953f94593050acd
karamage <karamage.umauma@gmail.com>
2021-01-01 17:04:34 +0900

    init two layer net.

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,6 @@
 import numpy as np
+from common.functions import *
+from common.util import im2col, col2im
 
 class Relu:
     def __init__(self):
@@ -30,3 +32,44 @@ class Sigmoid:
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
         return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 損失
+        self.y = None # softmaxの出力
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
+
